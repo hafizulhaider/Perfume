f62ef154bfec990a28d9f2db186b81c34d58eb38
devhaider <rthafizulhaider@gmail.com>
2024-09-19 10:49:47 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html', // Include HTML file
+    './src/**/*.{js,ts,jsx,tsx}' // Include JS/TS files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
