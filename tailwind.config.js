f1734587f7855f05502303667b920316188f48be
devhaider <rthafizulhaider@gmail.com>
2024-09-19 15:08:12 +0600

    navbar designing done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}' // Include JS/TS files in src folder
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1400px"
+    },
+    extend: {
+      colors: {
+        bodyColor:"hsl(300, 100%, 2%)",
+        secColor: "hsl(300, 100%, 5%)",
+        cardColor:"hsl(300, 100%, 10%)",
+        titleColor: "hsl(300, 100%, 23%)",
+        textColor: "hsl(0, 0%, 100%)",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "12px",
+        md: "32px",
+      },
+    },
+    fontFamily: {
+      Jost: ["Jost", "sans-serif"],
+      DancingScript: ["Dancing Script", "cursive"],
+    },
   },
   plugins: [],
 }
